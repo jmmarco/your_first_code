0db278054c1584cc55a847cc9cd487cecb7ede3f
Juan Marco <jmmarco@gmail.com>
2020-11-03 10:20:11 -0300

    Raise to 32 iterations, add 'hello' and  stdio header

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i <= 31; i++)
   {
-    printf("School\n");
+    printf("Hello School\n");
   }
   return 0;
 }
